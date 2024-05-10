033b47b4c8b4206fe65c98e775542b04bcd67438
nd92 <nd203@leicester.ac.uk>
2024-05-10 16:20:22 +0100

    Change GPIO class to physicalPin for better clarity

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -1,20 +1,7 @@
 #include "wiringPi.H"
-// #include "wiringPiNode.H"
 #include "dummyDevice.H"
-#include "GPIO.H"
 
 using namespace WiringPi;
-
-// inline constexpr pin_t h(const pin_t i)
-// {
-//     return 7 + (i / 64) + (i / 32);
-//     // return 7 + (i / 32);
-// }
-
-inline constexpr size_t g(const size_t i)
-{
-    return 13 + (i / 32);
-}
 
 int main(void)
 {
@@ -26,16 +13,6 @@ int main(void)
     wPi.addDevice(dummyDevice(8));
 
     wPi.printPinRegistry();
-
-    std::vector<wiringPiNode> f = {GPIO(0), dummyDevice(0), dummyDevice(8)};
-
-    for (size_t i = 0; i < 3; i++)
-    {
-        std::cout << f[i].deviceName() << ".digitalRead() = " << f[1].digitalRead() << std::endl;
-    }
-
-    std::cout << "wpi.readOnboard(1) = " << wPi.digitalReadOnboard(1) << std::endl;
-    // wPi.digitalWriteOnboard(63, 0);
 
     return 0;
 }
