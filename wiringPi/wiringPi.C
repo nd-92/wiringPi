27fc4337d4f594dab5fb7ba18f84b1b22f21aa27
nd92 <nd203@leicester.ac.uk>
2024-08-08 00:46:39 +0100

    Move hardware info to separate class

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -1,6 +1,4 @@
 #include "wiringPi.H"
-// #include "dummyDevice.H"
-// #include <cmath>
 
 using namespace WiringPi;
 
@@ -8,28 +6,16 @@ int main(void)
 {
     wiringPi wPi;
 
-    // for (pin_t pin = 0; pin < 32; pin++)
-    // {
-    //     const gpio_t j = wPi.digitalReadOnboard(pin);
-    //     std::cout << "digitalReadOnboard returned " << j << " from pin " << wPi.pinMap()[pin] << std::endl;
-    // }
-
-    // for (pin_t pin = 0; pin < 32; pin++)
-    // {
-    //     wPi.digitalWriteOnboard(pin, 0);
-    //     std::cout << "digitalWriteOnboard wrote " << 0 << " to pin " << wPi.pinMap()[pin] << std::endl;
-    // }
-
-    for (pin_t pin = 0; pin < 32; pin++)
-    {
-        wPi.digitalWriteOnboard(pin, 1);
-        std::cout << "digitalWriteOnboard wrote " << 1 << " to pin " << wPi.pinMap()[pin] << std::endl;
-    }
-
     for (pin_t pin = 0; pin < 32; pin++)
     {
         const gpio_t j = wPi.digitalReadOnboard(pin);
         std::cout << "digitalReadOnboard returned " << j << " from pin " << wPi.pinMap()[pin] << std::endl;
+    }
+
+    for (pin_t pin = 0; pin < 32; pin++)
+    {
+        wPi.digitalWriteOnboard(pin, 0);
+        std::cout << "digitalWriteOnboard wrote " << 0 << " to pin " << wPi.pinMap()[pin] << std::endl;
     }
 
     return 0;
