7fdfef66612092722eb102daa4d1a25e845121f8
nd92 <nd203@leicester.ac.uk>
2024-08-08 02:21:54 +0100

    Implements input/output pinMode for onboard pins

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -8,15 +8,21 @@ int main(void)
 
     for (pin_t pin = 0; pin < 32; pin++)
     {
-        const gpio_t j = wPi.digitalReadOnboard(pin);
-        std::cout << "digitalReadOnboard returned " << j << " from pin " << wPi.pinMap()[pin] << std::endl;
+        wPi.pinModeOnboard(pin, OUTPUT<int>());
+        // wPi.pinModeOnboard(pin, INPUT<int>());
     }
 
-    for (pin_t pin = 0; pin < 32; pin++)
-    {
-        wPi.digitalWriteOnboard(pin, 0);
-        std::cout << "digitalWriteOnboard wrote " << 0 << " to pin " << wPi.pinMap()[pin] << std::endl;
-    }
+    // for (pin_t pin = 0; pin < 32; pin++)
+    // {
+    //     const gpio_t j = wPi.digitalReadOnboard(pin);
+    //     std::cout << "digitalReadOnboard returned " << j << " from pin " << wPi.pinMap()[pin] << std::endl;
+    // }
+
+    // for (pin_t pin = 0; pin < 32; pin++)
+    // {
+    //     wPi.digitalWriteOnboard(pin, 0);
+    //     std::cout << "digitalWriteOnboard wrote " << 0 << " to pin " << wPi.pinMap()[pin] << std::endl;
+    // }
 
     return 0;
 }
