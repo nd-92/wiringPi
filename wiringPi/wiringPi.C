a0a4c16a773f3ee63f3b72a7499b95ec76b5ba5f
nd92 <nd203@leicester.ac.uk>
2024-08-11 06:10:09 +0100

    Move onboard checking to piHardwareInfo class

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -4,13 +4,29 @@ using namespace WiringPi;
 
 int main(void)
 {
-    wiringPi Pi;
+    // wiringPi<WPI_MODE_PINS()> Pi;
 
-    Pi.readAll();
+    wiringPi<WPI_MODE_GPIO()> Pi;
+
+    // Pi.readAll();
+
+    Pi.pinModeOnboard<31>(OUTPUT<gpio_t>());
+
+    // constexpr pin_t pinNumber = 40;
+
+    // const std::string toPrint = std::string("Pin number " + std::to_string(pinNumber) + " is not onboard (onboard pins 0 -> 31)");
+
+    // std::cout << toPrint << std::endl;
 
     // for (pin_t pin = 0; pin < 32; pin++)
     // {
     //     Pi.pinModeOnboard(pin, OUTPUT<gpio_t>());
+    // }
+
+    // for (pin_t pin = 0; pin < 32; pin++)
+    // {
+    // constexpr pin_t pin = 18;
+    // std::cout << "Digital read returned " << Pi.digitalReadOnboard<pin>() << " on pin " << pin << std::endl;
     // }
 
     // for (pin_t pin = 0; pin < 64; pin++)
