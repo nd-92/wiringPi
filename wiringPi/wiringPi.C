2baf8c07f4a49a7908ff9320ee7b7385826ebd57
nd92 <nd203@leicester.ac.uk>
2024-10-19 09:06:48 +0100

    Initialise sig handler in ctor

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -15,18 +15,44 @@ int main(void)
 
     // std::cout << "s_1 = " << s_1 << std::endl;
 
-    wiringPi<WPI_MODE_GPIO()> Pi;
+    // static constexpr const std::string s_1 = "Hello ";
 
-    // const gpio_t i = Pi.digitalReadOnboard<3>();
+    // static constexpr const std::string s_2 = "world!";
 
-    // std::cout << "i = " << i << std::endl;
+    // static constexpr const std::string s_3 = s_1 + s_2;
+
+    // std::cout << s_3 << std::endl;
+
+    wiringPi<WPI_MODE_PHYS()> Pi;
+
+    // while (Pi.run())
+    // {
+    //     ;
+    // }
+
+    const gpio_t i = Pi.digitalReadOnboard<3>();
+
+    std::cout << "i = " << i << std::endl;
 
     // for (size_t i = 0; i < 64; i++)
-    for (pinTable::const_iterator i = Pi.pinMap().begin(); i < Pi.pinMap().end(); i++)
-    {
-        // std::cout << "i = " << *i << ", pin = " << Pi.pinMap()[i] << std::endl;
-        std::cout << "i = " << *i << std::endl;
-    }
+    // for (pinTable::const_iterator i = Pi.pinMap().begin(); i < Pi.pinMap().end(); i++)
+    // {
+    //     // std::cout << "i = " << *i << ", pin = " << Pi.pinMap()[i] << std::endl;
+    //     std::cout << "i = " << *i << std::endl;
+    // }
+
+    // std::cout << "i = " << Pi.digitalReadOnboard<3>() << std::endl;
+
+    // struct sigaction sa
+    // {
+    //     signalHandler,
+    // };
+    // memset(&sa, 0, sizeof(sa));
+
+    // const struct sigaction sa = initialiseSigaction();
+
+    // std::cout << "sa.sa_mask = " << *sa.sa_mask.__val << std::endl;
+    // std::cout << "sa.sa_flags = " << sa.sa_flags << std::endl;
 
     return 0;
 }
