75608a1a5280c20460f8a272cc0677ffd9d385ea
nd92 <nd203@leicester.ac.uk>
2024-12-18 03:23:08 +0000

    Implement basic unit tests and static assertions

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -1,58 +1,73 @@
 #include "wiringPi.H"
+#include "argHandler.H"
 
 using namespace WiringPi;
 
-int main(void)
+int main(const int argc, const char *argv[])
 {
+    // Handle the input arguments
+    const argHandler args(argc, argv);
 
-    // static constexpr auto a1 = constexpr_strcat(
-    //     std::to_array("Hello"),
-    //     std::to_array(" "),
-    //     std::to_array("world"),
-    //     std::to_array("!"));
+    // Write the pin layout
+    if (args.pinLayout())
+    {
+        if ((args.wiringPiMode() == WPI_MODE_PINS()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_PINS()> Pi;
+            Pi.pinLayout();
+        }
+        if ((args.wiringPiMode() == WPI_MODE_GPIO()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_GPIO()> Pi;
+            Pi.pinLayout();
+        }
+        if ((args.wiringPiMode() == WPI_MODE_PHYS()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_PHYS()> Pi;
+            Pi.pinLayout();
+        }
+    }
 
-    // constexpr const std::string_view s_1 = toStringView(a1);
-
-    // std::cout << "s_1 = " << s_1 << std::endl;
-
-    // static constexpr const std::string s_1 = "Hello ";
-
-    // static constexpr const std::string s_2 = "world!";
-
-    // static constexpr const std::string s_3 = s_1 + s_2;
-
-    // std::cout << s_3 << std::endl;
-
-    wiringPi<WPI_MODE_PHYS()> Pi;
-
-    // while (Pi.run())
-    // {
-    //     ;
-    // }
-
-    const gpio_t i = Pi.digitalReadOnboard<3>();
-
-    std::cout << "i = " << i << std::endl;
-
-    // for (size_t i = 0; i < 64; i++)
-    // for (pinTable::const_iterator i = Pi.pinMap().begin(); i < Pi.pinMap().end(); i++)
-    // {
-    //     // std::cout << "i = " << *i << ", pin = " << Pi.pinMap()[i] << std::endl;
-    //     std::cout << "i = " << *i << std::endl;
-    // }
-
-    // std::cout << "i = " << Pi.digitalReadOnboard<3>() << std::endl;
-
-    // struct sigaction sa
-    // {
-    //     signalHandler,
-    // };
-    // memset(&sa, 0, sizeof(sa));
-
-    // const struct sigaction sa = initialiseSigaction();
-
-    // std::cout << "sa.sa_mask = " << *sa.sa_mask.__val << std::endl;
-    // std::cout << "sa.sa_flags = " << sa.sa_flags << std::endl;
+    // Perform the read/write unit test
+    if (args.readWriteTest())
+    {
+        if ((args.wiringPiMode() == WPI_MODE_PINS()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_PINS()> Pi;
+            if (Pi.readWriteTest() == unit_test_pass())
+            {
+                std::cout << "Unit test passed on mode PINS" << std::endl;
+            }
+            else
+            {
+                std::cout << "Unit test failed on mode PINS" << std::endl;
+            }
+        }
+        if ((args.wiringPiMode() == WPI_MODE_GPIO()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_GPIO()> Pi;
+            if (Pi.readWriteTest() == unit_test_pass())
+            {
+                std::cout << "Unit test passed on mode GPIO" << std::endl;
+            }
+            else
+            {
+                std::cout << "Unit test failed on mode GPIO" << std::endl;
+            }
+        }
+        if ((args.wiringPiMode() == WPI_MODE_PHYS()) | (args.wiringPiMode() == WPI_MODE_ALL()))
+        {
+            wiringPi<WPI_MODE_PHYS()> Pi;
+            if (Pi.readWriteTest() == unit_test_pass())
+            {
+                std::cout << "Unit test passed on mode PHYS" << std::endl;
+            }
+            else
+            {
+                std::cout << "Unit test failed on mode PHYS" << std::endl;
+            }
+        }
+    }
 
     return 0;
 }
