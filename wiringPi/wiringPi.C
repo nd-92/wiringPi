4621f8ab471530147545b1bf922ad8e06629aa03
nd92 <nd203@leicester.ac.uk>
2024-12-26 19:47:53 +0000

    Implement softPwm functionality

--- wiringPi/wiringPi.C
+++ wiringPi/wiringPi.C
@@ -8,6 +8,53 @@ int main(const int argc, const char *argv[])
     // Handle the input arguments
     const argHandler args(argc, argv);
 
+    // Test softPwm implementation
+    if (args.threadTest())
+    {
+        if ((args.wiringPiMode() == wiringPiModes::pins()) | (args.wiringPiMode() == wiringPiModes::all()))
+        {
+            // Define a pin and create the wiringPi object
+            constexpr const pin_t myPin = 31;
+            wiringPi<wiringPiModes::pins()> Pi;
+
+            // Create a softPwm thread
+            Pi.softPwm<myPin>(10, 100);
+
+            // End it after 2 seconds
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            Pi.softPwmEnd<myPin>();
+            std::cout << "Software PWM thread test on mode pins passed on pin " << myPin << std::endl;
+        }
+        if ((args.wiringPiMode() == wiringPiModes::gpio()) | (args.wiringPiMode() == wiringPiModes::all()))
+        {
+            // Define a pin and create the wiringPi object
+            constexpr const pin_t myPin = 31;
+            wiringPi<wiringPiModes::gpio()> Pi;
+
+            // Create a softPwm thread
+            Pi.softPwm<myPin>(10, 100);
+
+            // End it after 2 seconds
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            Pi.softPwmEnd<myPin>();
+            std::cout << "Software PWM thread test on mode gpio passed on pin " << myPin << std::endl;
+        }
+        if ((args.wiringPiMode() == wiringPiModes::phys()) | (args.wiringPiMode() == wiringPiModes::all()))
+        {
+            // Define a pin and create the wiringPi object
+            constexpr const pin_t myPin = 31;
+            wiringPi<wiringPiModes::phys()> Pi;
+
+            // Create a softPwm thread
+            Pi.softPwm<myPin>(10, 100);
+
+            // End it after 2 seconds
+            std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+            Pi.softPwmEnd<myPin>();
+            std::cout << "Software PWM thread test on mode phys passed on pin " << myPin << std::endl;
+        }
+    }
+
     // Write the pin layout
     if (args.pinLayout())
     {
